0ad1ef1fe63436b42782c02456fb6175400e3ec4
Opilane TTHK <opilane@tthk.ee>
2020-02-11 10:33:16 +0200

    Today and Birthday

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log("Hello World!")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+function day_of_the_week(date){
+    var days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    return days_of_week[date.getDay()];
+}
+
+app.get('/today', (req, res) => {
+    var today = new Date();
+    res.render("day_of_week",{
+        date: today,
+        days_of_week: day_of_the_week(today)
+    });
+})
+app.get('/birthday', (req, res) => {
+    var birthday = new Date(2002, 9, 5);
+    res.render("day_of_week",{
+        date: birthday,
+        days_of_week: day_of_the_week(birthday)
+    });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
